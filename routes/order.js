7119e1c7d722645e2e6a5f081090a1ca8930cbf0
Nikša <niksa.skeledzija@gmail.com>
2016-01-20 22:52:07 +0100

    Little update for order form and back end.

--- routes/order.js
+++ routes/order.js
@@ -4,11 +4,20 @@ var sessionUtils = require('../custom_modules/session-utils.js');
 module.exports.autoroute = {
 	get: {
 		'/order' : [sessionUtils.requireLogin, get]
+	},
+	post: {
+		'/order' : [sessionUtils.requireLogin, post]
 	}
 }
 
-/* GET dashboard */
+/* GET orders page */
 function get(req,res) {
 	console.log('Router works');
 	res.render('order');
+}
+
+
+/* POST order */
+function post(req,res) {
+	res.render('dashboard', {message: 'Order submitted successfully!'});
 }
